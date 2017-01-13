80d69b5e5053c2d9ae0d968c84f02eac871f709f
Michael Turnwall <freelance@turnwall.net>
2017-01-13 00:18:44 -0800

    Adding closest method

--- domassist.default.js
+++ domassist.default.js
@@ -11,6 +11,7 @@ import once from './lib/once';
 import removeClass from './lib/removeClass';
 import show from './lib/show';
 import matches from './lib/matches';
+import closest from './lib/closest';
 
 export default {
   addClass,
@@ -25,5 +26,6 @@ export default {
   once,
   removeClass,
   show,
-  matches
+  matches,
+  closest
 };
