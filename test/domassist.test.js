adf8cdc78cd8c758967c8a45284fbcbed3486ce5
Greg Allen <greg@firstandthird.com>
2017-01-19 12:51:28 -0800

    enhancements to toArray

--- test/domassist.test.js
+++ test/domassist.test.js
@@ -80,7 +80,28 @@ test('addClass, hasClass, removeClass, toggleClass', assert => {
   assert.end();
 });
 
-test('toArray', assert => {
+test('toArray - null', assert => {
+  const els = domassist.toArray();
+  assert.equal(Array.isArray(els), true, 'always returns an array');
+  assert.equal(els.length, 0, 'returns empty array if null');
+  assert.end();
+});
+
+test('toArray - single node', assert => {
+  const container = domassist.findOne('#domassist');
+
+  container.innerHTML = `
+    <div class="test"></div>
+  `;
+  const el = domassist.findOne('div', container);
+  const els = domassist.toArray(el);
+  assert.equal(Array.isArray(els), true, 'is array');
+  assert.equal(els.length, 1);
+  assert.equal(els[0], el);
+  assert.end();
+});
+
+test('toArray - nodelist', assert => {
   const frag = document.createDocumentFragment();
   const total = 5;
   for (let i = 0; i < total; i += 1) {
