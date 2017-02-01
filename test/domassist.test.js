859af5fe116d32448bd586a229120fa682a44389
Michael Turnwall <freelance@turnwall.net>
2017-02-01 01:38:05 -0800

    Move toArray tests to external file

--- test/domassist.test.js
+++ test/domassist.test.js
@@ -4,6 +4,7 @@ import domassist from '../domassist';
 import test from 'tape-rollup';
 import { teardown } from './setup';
 import './find.test.js';
+import './to-array.test.js';
 
 const page = window.phantom.page;
 
@@ -66,46 +67,6 @@ test('addClass, hasClass, removeClass, toggleClass', assert => {
   domassist.toggleClass(el.firstChild, 'new-class');
   assert.notOk(domassist.hasClass(el.firstChild, 'new-class'), 'toggleClass - class list should not contain "new-class"');
   // teardown
-  while (el.firstChild) {
-    el.removeChild(el.firstChild);
-  }
-  assert.end();
-});
-
-test('toArray - null', assert => {
-  const els = domassist.toArray();
-  assert.equal(Array.isArray(els), true, 'always returns an array');
-  assert.equal(els.length, 0, 'returns empty array if null');
-  assert.end();
-});
-
-test('toArray - single node', assert => {
-  const container = domassist.findOne('#domassist');
-
-  container.innerHTML = `
-    <div class="test"></div>
-  `;
-  const el = domassist.findOne('div', container);
-  const els = domassist.toArray(el);
-  assert.equal(Array.isArray(els), true, 'is array');
-  assert.equal(els.length, 1);
-  assert.equal(els[0], el);
-  assert.end();
-});
-
-test('toArray - nodelist', assert => {
-  const frag = document.createDocumentFragment();
-  const total = 5;
-  for (let i = 0; i < total; i += 1) {
-    const div = document.createElement('div');
-    domassist.addClass(div, 'div-array');
-    frag.appendChild(div);
-  }
-  const el = domassist.findOne('#domassist');
-  el.appendChild(frag);
-  const divs = domassist.find('.div-array');
-  const arr = domassist.toArray(divs);
-  assert.ok(Array.isArray(arr), 'Nodelist converted to an array');
   while (el.firstChild) {
     el.removeChild(el.firstChild);
   }
