344665943cfcf569f82447cee87732c8db48d805
Michael Turnwall <freelance@turnwall.net>
2017-02-01 11:53:32 -0800

    Finish updating on() to accept array of elements

--- test/on.test.js
+++ test/on.test.js
@@ -5,18 +5,15 @@ const page = window.phantom.page;
 
 test('Events - on single element', assert => {
   const el = domassist.findOne('#domassist');
-
+  assert.plan(1);
   el.innerHTML = `
     <a href="#">Click</a>
   `;
-  console.log(el);
-  const link = domassist.find('a');
-  console.log(`link ${typeof link}`);
+  const link = domassist.findOne('a');
   const pos = link.getBoundingClientRect();
 
   domassist.on(link, 'click', e => {
     assert.ok(e instanceof MouseEvent, 'Event fired');
-    assert.end();
   });
 
   page.sendEvent('click', pos.left + pos.width / 2, pos.top + pos.height / 2);
@@ -24,21 +21,22 @@ test('Events - on single element', assert => {
 
 test('Events - on multiple elements', assert => {
   const el = domassist.findOne('#domassist');
-
+  assert.plan(4);
   el.innerHTML = `
-    <a href="#">Click</a>
-    <a href="#">Click</a>
-    <a href="#">Click</a>
-    <a href="#">Click</a>
+    <a id="link-1" href="#">Click</a>
+    <a id="link-2" href="#">Click</a>
+    <a id="link-3" href="#">Click</a>
+    <a id="link-4" href="#">Click</a>
   `;
 
-  const link = domassist.find('a', el);
-  const pos = link.getBoundingClientRect();
+  const links = domassist.find('a', el);
+  links.forEach((item, index) => {
+    const pos = item.getBoundingClientRect();
 
-  domassist.on(link, 'click', e => {
-    assert.ok(e instanceof MouseEvent, 'Event fired');
-    assert.end();
+    domassist.on(item, 'click', e => {
+      assert.equal(e.target.id, `link-${index + 1}`, `Link with id of link-${index + 1} fired`);
+    });
+
+    page.sendEvent('click', pos.left + pos.width / 2, pos.top + pos.height / 2);
   });
-
-  page.sendEvent('click', pos.left + pos.width / 2, pos.top + pos.height / 2);
 });
