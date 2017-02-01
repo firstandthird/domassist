c989b9c8b55f3c5a409451a9ad2bf858f93cb0bf
Michael Turnwall <freelance@turnwall.net>
2017-02-01 01:18:45 -0800

    Create addAttrs() method

---
+++ test/attrs.test.js
@@ -0,0 +1,58 @@
+import domassist from '../domassist';
+import test from 'tape-rollup';
+
+const setup = (total) => {
+  const frag = document.createDocumentFragment();
+  for (let i = 0; i < total; i += 1) {
+    const div = document.createElement('div');
+    domassist.addClass(div, 'test-divs');
+    domassist.addClass(div, `div-${i}`);
+    const p = document.createElement('p');
+    p.innerHTML = `paragraph-${1}`;
+    div.appendChild(p);
+    frag.appendChild(div);
+  }
+  return frag;
+};
+
+const teardown = (el) => {
+  while (el.firstChild) {
+    el.removeChild(el.firstChild);
+  }
+};
+
+test('attrs - div attributes', assert => {
+  const el = domassist.findOne('#domassist');
+  el.appendChild(setup(5));
+  let testDiv = domassist.findOne('.div-0');
+  domassist.addAttrs(testDiv, {
+    id: 'new-id',
+    title: 'this is a title',
+    testAttr: 'data attribute',
+  });
+  testDiv = domassist.findOne('.div-0');
+  assert.equal(testDiv.id, 'new-id', 'ID attribute added');
+  assert.equal(testDiv.title, 'this is a title', 'Title attribute added');
+  assert.equal(testDiv.dataset.testAttr, 'data attribute', 'Data attribute added');
+  teardown(el);
+  assert.end();
+});
+
+test('attrs - anchor attributes', assert => {
+  const el = domassist.findOne('#domassist');
+  el.appendChild(document.createElement('a'));
+  let testAnchor = domassist.findOne('a');
+  domassist.addAttrs(testAnchor, {
+    id: 'anchor-id',
+    title: 'this is a title',
+    href: 'http://google.com',
+    testAttr: 'data attribute',
+  });
+  testAnchor = domassist.findOne('a');
+  assert.equal(testAnchor.id, 'anchor-id', 'ID attribute added');
+  assert.equal(testAnchor.title, 'this is a title', 'Title attribute added');
+  assert.equal(testAnchor.href, 'http://google.com/', 'HREF attribute added');
+  assert.equal(testAnchor.dataset.testAttr, 'data attribute', 'Data attribute added');
+  teardown(el);
+  assert.end();
+});
